86552dbe16d8def4b399757cebe0e66e7016399a
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-06-12 17:27:07 +0530

    Change Default User Model
    - Add booking App in Installed Apps
    - Change Default User model to Booking's Custom User Model

--- ConferenceRoomBookingSystem/settings.py
+++ ConferenceRoomBookingSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booking',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -104,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom Settings
+AUTH_USER_MODEL = 'booking.User'
