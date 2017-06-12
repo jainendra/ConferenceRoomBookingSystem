f72d6e764c975852f753dca7b5d1a58bbee71e72
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-06-12 17:28:40 +0530

    Register booking's User model into Django-Admin's Interface
    - Change UserAdmin to exclude username field

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from booking.models import User, Admin, Room, Booking
+
+UserAdmin.ordering = ('email',)
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+UserAdmin.fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+UserAdmin.add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Admin)
+admin.site.register(Room)
+admin.site.register(Booking)
