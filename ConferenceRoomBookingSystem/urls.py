91b42d8c6edb8b7000e4991efaa0df9b4f9e425f
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-06-13 18:25:43 +0530

    Add Booking App URLs
    - Add Urls for home, index, signup, login, logout
    - Remove URL for api

--- ConferenceRoomBookingSystem/urls.py
+++ ConferenceRoomBookingSystem/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from booking.api.api import api
-
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/$', api, name='api')
+    url(r'^', include('booking.urls', namespace='booking')),
+    url(r'^admin/', include(admin.site.urls))
 ]
