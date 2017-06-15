d05c6ce728a6ce876cf9e7ed5ba8231eed92534d
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-06-15 12:28:48 +0530

    Add login and signup view functions
    - Remove index view function
    - Add login and signup view fucntions

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, Http404
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
@@ -6,61 +6,52 @@ from django.contrib.auth.forms import AuthenticationForm
 from forms import SignupForm
 
 
-def index(request):
+def signup(request):
     """
     This function
-        - generates view for landing page of CRBS for a non-authenticated/not-logged in user
-        - contain login and signup page
+        - returns view for signup page
+        - handles http request for signup
+        - creates new user
     :param request: HttpRequest
     :return: HttpResponseObject
     """
     user = request.user
     if user.is_authenticated():
         return redirect('booking:home')
-    else:
-        login_form = AuthenticationForm(prefix='login', auto_id='id_login_%s')
-        signup_form = SignupForm(prefix='signup', auto_id='id_signup_%s')
-        context = {
-            'login_form': login_form,
-            'signup_form': signup_form
-        }
-        return render(request, 'index.html', context)
-
-
-def signup(request):
-    """
-    This function
-        - handles post request for signup
-        - creates new user
-        - logs in the new user
-    :param request: HttpRequest
-    :return: HttpResponseObject
-    """
     if request.method == 'POST':
         signup_form = SignupForm(data=request.POST, prefix='signup', auto_id='id_signup_%s')
+        message = ''
         if signup_form.is_valid():
             if signup_form.save():
-                # TODO: Add login code and confirmation message for signed In user
-                return redirect('booking:index')
+                message = 'You account has been created, you may login with your entered credentials'
             else:
-                return redirect('booking:index')
+                message = 'You account has not been created, some error occurred'
 
+        context = {
+            'signup_form': signup_form,
+            'message': message
+        }
+        return render(request, 'signup.html', context)
+    else:
+        signup_form = SignupForm(prefix='signup', auto_id='id_signup_%s')
         context = {
             'signup_form': signup_form
         }
-        return render(request, 'index.html', context)
-    else:
-        raise Http404
+        return render(request, 'signup.html', context)
 
 
 def login(request):
     """
     This function
-        - handles post request for login
+        - returns view for login page
+        - handles http request for login
         - logs in user with correct credentials
     :param request: HttpRequest
     :return: HttpResponseObject
     """
+    user = request.user
+    if user.is_authenticated():
+        return redirect('booking:home')
     if request.method == 'POST':
         login_form = AuthenticationForm(data=request.POST, prefix='login', auto_id='id_login_%s')
 
@@ -75,16 +66,17 @@ def login(request):
                 auth.login(request, user)
                 return redirect('booking:home')
 
-        signup_form = SignupForm(prefix='signup', auto_id='id_signup_%s')
-
         context = {
-            'login_form': login_form,
-            'signup_form': signup_form
+            'login_form': login_form
         }
 
-        return render(request, 'index.html', context)
+        return render(request, 'login.html', context)
     else:
-        raise Http404
+        login_form = AuthenticationForm(prefix='login', auto_id='id_login_%s')
+        context = {
+            'login_form': login_form
+        }
+        return render(request, 'login.html', context)
 
 
 @login_required
@@ -97,7 +89,7 @@ def logout(request):
     :return: HttpResponseObject
     """
     auth.logout(request)
-    return redirect('booking:index')
+    return redirect('booking:login')
 
 
 @login_required
